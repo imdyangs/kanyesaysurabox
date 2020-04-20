c6d07328b2cde979a560a78f5adb12ed1417aed1
imdyangs <d65yang@gmail.com>
2020-04-19 20:28:05 -0600

    editted timings

--- script.js
+++ script.js
@@ -59,20 +59,20 @@ const callback = () => {
   btn.style.display = "none";
   vid.play();
 
-  setTimeout(() => setRandomDisplayPosition(a), 2940);
-  setTimeout(() => setRandomDisplayPosition(b), 6200);
-  setTimeout(() => setRandomDisplayPosition(c), 13500); // penis too small @ u
-  setTimeout(() => setRandomDisplayPosition(f1), 14500); // ego
-  setTimeout(() => setRandomDisplayPosition(f2), 14700); // ego
-  setTimeout(() => setRandomDisplayPosition(f3), 15000); // ego
-  setTimeout(() => setRandomDisplayPosition(f4), 15300); // ego
-  setTimeout(() => setRandomDisplayPosition(f5), 16000); // ego
-  setTimeout(() => setRandomDisplayPosition(e), 23500); //rsu backload
-  setTimeout(() => setRandomDisplayPosition(d), 24600); // 30k
-  setTimeout(() => setRandomDisplayPosition(power), 40800); // NO ONE MAN
-  setTimeout(() => setRandomDisplayPosition(g), 49500);
-  setTimeout(() => setRandomDisplayPosition(h), 50000); // earf go hard
-  setTimeout(() => setRandomDisplayPosition(i), 60000); // epstein lol
+  setTimeout(() => setRandomDisplayPosition(a), 3140);
+  setTimeout(() => setRandomDisplayPosition(b), 6400);
+  setTimeout(() => setRandomDisplayPosition(c), 13700); // penis too small @ u
+  setTimeout(() => setRandomDisplayPosition(f1), 14700); // ego
+  setTimeout(() => setRandomDisplayPosition(f2), 14900); // ego
+  setTimeout(() => setRandomDisplayPosition(f3), 15200); // ego
+  setTimeout(() => setRandomDisplayPosition(f4), 15600); // ego
+  setTimeout(() => setRandomDisplayPosition(f5), 16200); // ego
+  setTimeout(() => setRandomDisplayPosition(e), 23700); //rsu backload
+  setTimeout(() => setRandomDisplayPosition(d), 24800); // 30k
+  setTimeout(() => setRandomDisplayPosition(power), 41000); // NO ONE MAN
+  setTimeout(() => setRandomDisplayPosition(g), 49700);
+  setTimeout(() => setRandomDisplayPosition(h), 52000); // earf go hard
+  setTimeout(() => setRandomDisplayPosition(i), 62000); // epstein lol
   setTimeout(endKanye, 65000);
 
   document.getElementById("fucku").removeEventListener("click", callback, false);
@@ -95,7 +95,7 @@ const callback = () => {
         document.getElementById("fucku").addEventListener("click", callback, false);
         wat.style.opacity = 0;
         wat2.style.opacity = 0;
-      }, 20000);
+      }, 1000);
     }
   })
 })();
